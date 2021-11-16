568735f0d1c0ca8d2f0561c7e7d52c88b05e4a6b
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-11-15 23:40:09 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,57 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        arreglo: [1,2,3,4,5,6,7,8,9,10]
+        titulo: "Directivas condicionales",
+        modoOscuro: false,
+        ventana: false,
+        tareas: [
+            {
+                titulo: "Comprar leche",
+                descripcion: "Comprar leche en la tienda",
+                prioridad: true
+            },
+            {
+                titulo: "Comprar azucar",
+                descripcion: "Comprar azucar en la tienda",
+                prioridad: false
+            },
+            {
+                titulo: "Comprar cerveza",
+                descripcion: "Comprar cerveza en la tienda",
+                prioridad: true
+            }
+        ],
+        inputTitulo: "",
+        inputDescripcion: "",
+        inputImportante: false,
+        tableHeaders: ['Numero', 'Titulo', 'Descripcion', 'Prioridad'],
+        tableItems: [
+            {
+                numero: 1,
+                titulo: "Comprar leche",
+                descripcion: "Comprar leche en la tienda",
+            },
+            {
+                numero: 2,
+                titulo: "Comprar leche",
+                descripcion: "Comprar leche en la tienda",
+            }
+        ]
     },
     methods: {
-        alert(){
-            alert(this.data.message);
-        }
-    },
-    computed: {
-        mensaje: function(){
-           return this.message.toUpperCase();
+        cambioFondo(){
+            this.modoOscuro = !this.modoOscuro;
+        },
+        guardarTarea(){
+           
+            const nuevaTarea = {
+                titulo: this.inputTitulo,
+                descripcion: this.inputDescripcion,
+                prioridad: this.inputImportante
+            }
+            console.log(nuevaTarea);
+            this.tareas.push(nuevaTarea);
         }
     }
+  
 });
